bb37b2f3fce62b54535134674928b2515dd32665
ronan-f <rfegan13@gmail.com>
2019-05-16 15:34:03 -0700

    Setting up input form for a user to add a song title. Also adding a state handler for text input

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { EventEmitter } from 'events';
 
 class SongCreate extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            title: ''
+        }
+    }
     render() {
         return (
             <div>
                 <h3>Create a new song</h3>
                 <form>
-
+                    <label>Song title:</label>
+                    <input onChange={event => this.setState({title: event.target.value})}/>
                 </form>
             </div>
         )
