465e1badd93dbc887e2b8a48d56b722abe64c0d1
ronan-f <rfegan13@gmail.com>
2019-05-16 15:01:21 -0700

    Adding a SongCreate component and creating a route from index.js

---
+++ client/components/SongCreate.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SongCreate extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Create a new song</h3>
+                <form>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SongCreate;
