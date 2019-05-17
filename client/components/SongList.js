a8d85e536e4f076fbb0dfebcf98b5eba9d60f716
ronan-f <rfegan13@gmail.com>
2019-05-16 21:36:15 -0700

    Adding delete mutation to allow users to remove songs

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -30,4 +30,14 @@ class SongList extends Component {
     }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+    mutation DeleteSong($id: ID){
+        deleteSong(id: $$id){
+            id
+        }
+    }
+`;
+
+export default graphql(mutation)(
+    graphql(query)(SongList)
+);
