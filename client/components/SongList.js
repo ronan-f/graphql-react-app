8e470fd2f8a41e1fd03c07a71b1c459b472946c5
ronan-f <rfegan13@gmail.com>
2019-05-16 19:44:36 -0700

    Adding button at bottom of songlist which links to /songs/new

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
 
@@ -18,7 +19,12 @@ class SongList extends Component {
             return <div>Loading...</div>
         }
         return (
-            <ul className="collection"> {this.renderSongs()} </ul>
+            <div>
+                <ul className="collection"> {this.renderSongs()} </ul>
+                <Link to="/songs/new" className="btn-floating btn-large blue right">
+                    <i className="material-icons">add</i>
+                </Link>
+            </div>
         );
     }
 }
